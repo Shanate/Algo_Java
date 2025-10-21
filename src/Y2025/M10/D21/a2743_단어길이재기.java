445e6fa01041d0b0860cbd4fdf15084b509974b9
shanate <redeyes5240@gmail.com>
2025-10-21 12:07:45 +0900

    B5

---
+++ src/Y2025/M10/D21/a2743_단어길이재기.java
@@ -0,0 +1,13 @@
+package Y2025.M10.D21;
+
+import java.io.*;
+import java.util.*;
+
+public class a2743_단어길이재기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        System.out.println(word.length());
+
+    }
+}
