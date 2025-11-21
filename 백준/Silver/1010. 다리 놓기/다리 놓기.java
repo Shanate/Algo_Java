60a9cf84b53e5e55aa92586e533161875e77cf4a
Shanate <109050481+Shanate@users.noreply.github.com>
2025-11-21 13:54:47 +0900

    [Silver V] Title: 다리 놓기, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[][] dp = new int[30][30];
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int T = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            sb.append(combi(M, N)).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+
+    static int combi(int n, int r) {
+
+        if (dp[n][r] > 0) {
+            return dp[n][r];
+        }
+        if (n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+
+        return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+    }
+
+
+}
