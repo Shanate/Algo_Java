0c8dec835cc1f73e6b03ccd6b16f4e17d4fe6370
Shanate <109050481+Shanate@users.noreply.github.com>
2025-11-27 13:16:44 +0900

    [Bronze II] Title: 뒤집힌 덧셈, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int rev1 = rev(x);
+        int rev2 = rev(y);
+
+        int addRev = rev1 + rev2;
+
+        int ans = rev(addRev);
+        System.out.println(ans);
+    }
+
+    static int rev(int input) {
+        String str = Integer.toString(input);
+
+        StringBuffer sb = new StringBuffer(str);
+        String reverse = sb.reverse().toString();
+
+        int revValue = Integer.parseInt(reverse);
+
+        return revValue;
+    }
+
+
+}
