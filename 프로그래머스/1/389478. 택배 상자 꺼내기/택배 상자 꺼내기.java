5b4ce57bde0f2c55f7bbdcd053ebbad0350d9fbd
Shanate <109050481+Shanate@users.noreply.github.com>
2025-12-22 16:47:46 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.06 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int solution(int n, int w, int num) {
+int answer = 0;
+        int height = (n / w) + 1;
+        boolean left = true;
+        int[][] arr = new int[height][w];
+        int cnt = 0;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < w; j++) {
+                if (left == true && cnt < n) {
+                    arr[i][j] = j + 1;
+                    cnt++;
+                }
+                if (left == false && cnt < n) {
+                    arr[i][w - j - 1] = j + 1;
+                    cnt++;
+                }
+            }
+            left = !left;
+        }
+
+        int curHeight = (num - 1) / w;
+        int curPos = (num - 1) % w + 1; // 1부터 w까지 범위 유지
+
+        // 홀수층이면 반대 위치 적용
+        if (curHeight % 2 == 1) {
+            curPos = w - curPos + 1;
+        }
+
+        for (int i = curHeight; i < height; i++) {
+            if (arr[i][curPos - 1] != 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
